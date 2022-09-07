e85bb9a6d3b3f1041d12a9d6824581f413ad68c7
Veronika-chenko <99816537+Veronika-chenko@users.noreply.github.com>
2022-09-07 11:56:51 +0000

    Deploying to gh-pages from @ Veronika-chenko/practice-js@cd52a9c3bb8b95c91c15691b368945dae2b35723 🚀

---
+++ 01-running-button.640fa83d.js
@@ -0,0 +1,2 @@
+({container:document.querySelector(".btn-container")}).container.addEventListener("mouseover",(function(t){const{target:n}=t;n.style.top=`${Math.floor(Math.random()*(window.innerHeight-n.clientHeight))}px`,n.style.left=`${Math.floor(Math.random()*(window.innerWidth-n.clientWidth))}px`}));
+//# sourceMappingURL=01-running-button.640fa83d.js.map
---
+++ 01-running-button.b5984ab8.js
@@ -0,0 +1,2 @@
+document.querySelector(".btn-container").addEventListener("mouseover",(function(t){var e=t.target;e.style.top="".concat(Math.floor(Math.random()*(window.innerHeight-e.clientHeight)),"px"),e.style.left="".concat(Math.floor(Math.random()*(window.innerWidth-e.clientWidth)),"px")}));
+//# sourceMappingURL=01-running-button.b5984ab8.js.map
